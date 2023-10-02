5b1b9c84aa224771b9cd60e4a17241a2c18be37b
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2023-10-02 14:20:57 -0600

    sum of products should be correct in order amount

--- cypress/integration/examples/TCSuiteE2E.ts
+++ cypress/integration/examples/TCSuiteE2E.ts
@@ -17,7 +17,7 @@ describe("E2E Test", () => {
         cy.get(":nth-child(3) > strong").should("contain", "@currentPrice");    
         })
 
-    it.only("should make sure the order can be completed", () => {
+    it("should make sure the order can be completed", () => {
         cy.get(":nth-child(2) > .nav-link").click(); 
         cy.get(".card").find(".card-title").each((el, index) => {
             let elText = el.text(); 
@@ -36,5 +36,58 @@ describe("E2E Test", () => {
         cy.get("#checkbox2").should("be.checked");  
         cy.get(".btn-lg").click(); 
         cy.get(".alert").should("be.visible").and("contain", "Success! Thank you!");
-        })    
+        })
+        
+    it.only("should make sure the order amount is correct when more than one product is added", () => {
+        cy.get(".nav-link[href='/angularpractice/shop']").click();
+        cy.get(".h-100 .card-title").each((el, index) => {
+            if(el.text().includes("iphone") || el.text().includes("Samsung")){
+                cy.get(".h-100 .btn-info").eq(index).click();
+            }
+        })
+
+        cy.get(".nav-item .btn-primary").click();
+        
+        let pricePerItem:string[] = [];
+        let totalPrice:string[] = [];
+        let sum: number = 0;
+        cy.get("tr td:nth-child(4) strong").each(el => {
+            let priceWithSign = el.text();
+            let items = priceWithSign.split(""); 
+            let priceOnly:number[] = [];
+
+            items.forEach(value => { 
+                if(!isNaN(Number(value))){
+                    priceOnly.push(Number(value))
+                }
+            })
+            
+            if(priceOnly[0] === 0){
+                pricePerItem.push(priceOnly.join(""));    
+                pricePerItem.forEach(value => {  
+                    if(pricePerItem.length !== 1){ 
+                        sum = sum + Number(value);
+                    }
+                })  
+                cy.get(".text-right strong").each(el => {  
+                    let totalItem = el.text().split("");
+                    let totalPriceOnly:number[] = []; 
+                    totalItem.forEach(value => {
+                        if(!isNaN(Number(value))){
+                            totalPriceOnly.push(Number(value));
+                        }  
+                    })  
+                    if(totalPriceOnly[0] === 0){
+                        totalPrice.push(totalPriceOnly.join(""));   
+                        expect(sum).to.eq(Number(totalPrice[0]));
+
+                    }
+                })  
+
+            }    
+        }) 
+        
+    })    
+    
+        
     }) 
