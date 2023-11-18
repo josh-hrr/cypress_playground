dacb259b4ad5f0195efa9a9ae1d3d32556d06961
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2023-11-17 22:58:02 -0600

    footer test suite, added subscription validation in cart page

--- cypress/integration/examples/TCFooter.ts
+++ cypress/integration/examples/TCFooter.ts
@@ -1,10 +1,19 @@
 describe("Footer test suite", () => {
     beforeEach(() => {
         cy.visit("http://automationexercise.com"); 
-    })
-    it("Verify Subscription in home page", () => {
         cy.get(".col-sm-6 h1").should("contain", "AutomationExercise");
+    })
+    it("Verify Subscription in home page", () => { 
         cy.get("footer").scrollIntoView();
+        cy.get(".single-widget h2").should("contain", "Subscription");
+        cy.get("input[type='email']").should("have.attr", "placeholder", "Your email address");
+        cy.get("input[type='email']").type("test@test.com");
+        cy.get("button[type='submit']").click(); 
+        cy.get(".alert-success").should("contain", "You have been successfully subscribed!"); 
+    })
+
+    it("Verify Subscription in Cart page", () => {
+        cy.get("li a[href='/view_cart']").click();
         cy.get(".single-widget h2").should("contain", "Subscription");
         cy.get("input[type='email']").should("have.attr", "placeholder", "Your email address");
         cy.get("input[type='email']").type("test@test.com");
